149ba18821ffb8296dcc98d32d7e35386abe87b4
Ludwig Nussel <ludwig.nussel@suse.de>
2015-09-23 17:14:37 +0200

    create snapshot after firstboot

--- files/lib/kiwi/hooks/preCallInit.sh
+++ files/lib/kiwi/hooks/preCallInit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -x
+. /usr/lib/jeos-firstboot-functions
 rootfs="`stat -f -c %T /`"
 if [ "$rootfs" = 'btrfs' ]; then
 	echo "creating initial snapper config ..."
@@ -8,24 +9,5 @@ if [ "$rootfs" = 'btrfs' ]; then
 	# add fstab entry for .snapshots
 	sed -i -e '/@\/tmp/{p;s/tmp/.snapshots/g}' /etc/fstab
 	mount .snapshots
-	# we can't use snapper here due to missing dbus daemon in
-	# firstboot. So we have to create a snapshot manually.
-	#snapper --no-dbus -v create -d "Factory Status" --userdata "important=yes"
-	mkdir .snapshots/1
-	btrfs subvolume snapshot / .snapshots/1/snapshot
-	now="`date '+%Y-%m-%d %H:%M:%S'`"
-	cat > .snapshots/1/info.xml <<-EOF
-	<?xml version="1.0"?>
-	<snapshot>
-	  <type>single</type>
-	  <num>1</num>
-	  <uid>0</uid>
-	  <date>$now</date>
-	  <description>Factory status</description>
-	  <userdata>
-	    <key>important</key>
-	    <value>yes</value>
-	  </userdata>
-	</snapshot>
-	EOF
+	create_snapshot 1 "Factory status"
 fi
