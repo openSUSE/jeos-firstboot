591a81853558fa8bdcbb133e936216e2e9dcd300
Richard Brown <rbrownccb@opensuse.org>
2015-12-09 14:22:24 +0100

    Scripted reset/update of openSUSE jeos-firstboot based on SLE jeos-firstboot

--- files/lib/kiwi/hooks/preCallInit.sh
+++ files/lib/kiwi/hooks/preCallInit.sh
@@ -11,3 +11,12 @@ if [ "$rootfs" = 'btrfs' ]; then
 	mount .snapshots
 	create_snapshot 1 "Factory status"
 fi
+zypper ar -f http://download.opensuse.org/distribution/leap/42.1/repo/oss/ repo-oss
+zypper ar -f http://download.opensuse.org/distribution/leap/42.1/repo/non-oss/ repo-non-oss
+zypper ar -f http://download.opensuse.org/update/leap/42.1/oss/ repo-update-oss
+zypper ar -f http://download.opensuse.org/update/leap/42.1/non-oss/ repo-update-non-oss
+# Fix btrfs subvolumes
+chattr +C /var/lib/mysql
+chattr +C /var/lib/mariadb
+chattr +C /var/lib/pgsql
+chattr +C /var/lib/libvirt/images
