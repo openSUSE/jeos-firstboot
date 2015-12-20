820f05ddd4b517e364750c256b5f7e174d412c07
Michal Nowak <mnowak@suse.com>
2015-12-20 14:13:22 +0100

    Enhance altering of VM images with changes necessary to skip firstboot.

--- contrib/patch-jeos-image-with-autofirstboot.sh
+++ contrib/patch-jeos-image-with-autofirstboot.sh
@@ -6,7 +6,7 @@ clean(){
   rmmod nbd
 }
 
-#set -x
+set -x
 set -e
 
 # Defaults to zero, or one, respectively, unless set explicitly
@@ -14,32 +14,50 @@ DEV=${DEV-0}
 PARTITION=${PARTITION-1}
 IMG="$1"
 MNT="$2"
-PATCHES="$3"
+CONTRIB="$3"
 
-if [ $# -lt 3 ]; then
+if [ $# -ne 3 ]; then
   echo "There has to be three arguments"
-  echo "Usage: $0 <image> <mountpoint> <patches>"
+  echo "  Usage: $0 <VM image> <dir mountpoint> <contrib-root>"
+  echo "  Example of contrib-root structure:
+    contrib/
+    ├── etc
+    │   └── YaST2
+    │       └── licenses
+    │           └── base
+    │               └── license.txt
+    ├── jeos-firstboot.patch
+    ├── root
+    │   └── add_jeos_qa_fakes.patch
+    └── usr
+        └── lib
+            ├── jeos-firstboot
+            └── jeos-firstboot-functions"
   exit 1
 fi
 
-if ! [ -r "$IMG" -a -d "$MNT" -a -r "$PATCHES" ]; then
+echo "VM image file : $IMG"
+echo "Mountpoint dir: $MNT"
+echo "Contrib-root  : $CONTRIB" # ← contributed patches, and files here
+if ! [ -r "$IMG" -a -d "$MNT" -a -d "$CONTRIB" ]; then
   echo "One of following files either does not exist,"
   echo "or is not readable, or is not writable:"
-  echo "  Image:                       $IMG"
-  echo "  Mountpoint:                  $MNT"
-  echo "  Patches (firstboot, others): $PATCHES"
   exit 2
 fi
 
 modprobe nbd max_part="10"
 qemu-nbd -c /dev/nbd${DEV} "$IMG"
 mount /dev/nbd${DEV}p${PARTITION} $MNT
-[ ! -e "$MNT"/etc/YaST2/licenses/base/license.txt ] && \
-  mkdir -p "$MNT"/etc/YaST2/licenses/base/ && \
-  touch "$MNT"/etc/YaST2/licenses/base/license.txt
-rm -fv "$MNT"/etc/YaST2/licenses/base/no-acceptance-needed
 trap clean EXIT
-for patch in `seq 3 $#`; do
-  eval epatch=\$$patch
-  patch -d "$MNT" -p1 < $epatch
+
+# prepare the system
+rm -fv "$MNT"/etc/YaST2/licenses/base/no-acceptance-needed
+# Copy files/dirs first, then patch them
+for file in `ls --group-directories-first ${CONTRIB}`; do
+  if [[ "$file" =~ .*\.patch$ ]]; then
+    patch -d "$MNT" -p1 < "$CONTRIB"/"$file"
+  else
+    # Do not overwrite existing files
+    cp -nvr "$CONTRIB""$file" "$MNT"
+  fi
 done
